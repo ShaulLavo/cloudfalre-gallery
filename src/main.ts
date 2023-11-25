0f444e7f12ef3c2bc31568d7bf4f43a93ee3cda5
Shaul Lavochkin <shaul919191@gmail.com>
2023-11-26 00:52:18 +0200

    pref test

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,17 @@ import { initializeScrollbar } from './scrollBar';
 let isInfinite = false;
 
 const init = async () => {
+	const start = performance.now();
 	lenisManager.initLenis({ isInfinite });
 	await renderGallery('app');
 	initImageAnime();
 	!isInfinite && initializeScrollbar();
+	const end = performance.now();
+	const elapsed = end - start;
+
+	console.log(`took ${elapsed} milliseconds to init.`);
 };
+
+init();
 
 init();

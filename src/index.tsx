5541a7654146a0c76af2469e2dbc01a7f17ad51e
Shaul Lavochkin <shaul919191@gmail.com>
2023-11-25 21:57:33 +0200

    title

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ app.get('/', (c) => {
       <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>☠</text></svg>">
       <link rel="stylesheet" href="/static/style.css">
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Vite + TS</title>
+      <title>CloudFlare ??</title>
     </head>
     <body>
       <div id="app"></div>
